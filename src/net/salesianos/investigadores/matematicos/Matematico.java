0d626e61d146df687987bea8a0ec8aff7c651767
usr3498 <usr3498@salesianos-lacuesta.net>
2026-05-28 14:00:36 +0100

    Update Matematico.java
    add EstadoSueldo for matematicos.

--- src/net/salesianos/investigadores/matematicos/Matematico.java
+++ src/net/salesianos/investigadores/matematicos/Matematico.java
@@ -1,14 +1,27 @@
-    package net.salesianos.investigadores.matematicos;
-    import net.salesianos.investigadores.Investigadores;
+package net.salesianos.investigadores.matematicos;
+import net.salesianos.investigadores.Investigadores;
 
-    public class Matematico extends Investigadores {
+public class Matematico extends Investigadores {
 
-        public int[][][] matriz;
+    public int[][] matriz;
 
-        public Matematico(String nombre, String especialidad, int sueldo){
-            super(nombre, especialidad, sueldo);
-            this.matriz = new int[1][3][3];
-        }
-
-
+    public Matematico(String nombre, String especialidad, int sueldo) {
+        super(nombre, especialidad, sueldo);
+        this.matriz = new int[3][3];
     }
+
+    @Override
+    public void EstadoSueldo(){
+        if (getSueldo() < 1800) {
+            System.out.println("Jefe quiero un aumento para el lego batman");
+        } else{
+            System.out.println("Agusto con la paga boss");
+        }
+    }
+
+    @Override
+    public void Trabajar(){
+        
+    }
+
+}
